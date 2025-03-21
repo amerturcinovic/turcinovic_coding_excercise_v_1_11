8b6b05485998956d6efd2b20b3ee456d375f704d
Amer Turcinovic <amer.turcinovic@outlook.com>
2025-03-21 15:04:40 +0100

    happy path test added

--- test/impl/FootballScoreBoardTest.java
+++ test/impl/FootballScoreBoardTest.java
@@ -1,12 +1,26 @@
 package impl;
 
+import models.MatchInfo;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FootballScoreBoardTest {
     @Test
-    public void happyPath() {
-        assertTrue(true);
+    public void whenMatchStartedExpectToShowOnBoardSummary() {
+        var footballScoreBoard = new FootballScoreBoard();
+
+        footballScoreBoard.startMatch("BRAZIL", "ARGENTINA");
+
+        assertEquals(
+                List.of(
+                        new MatchInfo(
+                                "ARGENTINA", 0, "BRAZIL", 0
+                        )
+                ),
+                footballScoreBoard.getBoardSummary()
+        );
     }
 }

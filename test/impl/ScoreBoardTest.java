efd63f2d9e6c29587646ecb6b55c4a4e170de61e
Amer Turcinovic <amer.turcinovic@outlook.com>
2025-03-21 14:01:23 +0100

    setup java project lib

---
+++ test/impl/ScoreBoardTest.java
@@ -0,0 +1,12 @@
+package impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ScoreBoardTest {
+    @Test
+    public void happyPath() {
+        assertTrue(true);
+    }
+}
